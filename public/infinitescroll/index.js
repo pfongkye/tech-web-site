0f0200543a2592dd03e6bbe4fd76ff27767bbea4
Pascal Fong Kye <pfongkye@yahoo.com>
2020-11-02 21:35:44 +0000

    fix: use documentfragment

--- public/infinitescroll/index.js
+++ public/infinitescroll/index.js
@@ -1,15 +1,20 @@
 window.onload = async function () {
     const result = await fetch("https://api.jikan.moe/v3/manga/1/characters");
     const mangaChars = await result.json();
-    const images = mangaChars.characters.map(c => {
+    //a document fragment is stored in memory, and not added to DOM tree -> perf optimization
+    //https://developer.mozilla.org/en-US/docs/Web/API/Document/createDocumentFragment
+    const docFragment = document.createDocumentFragment();
+
+    mangaChars.characters.forEach(c => {
         const thumbnail = document.createElement("div");
         const caption = document.createElement("caption");
         caption.appendChild(document.createTextNode(c.name));
         const img = document.createElement("img");
         img.setAttribute("alt", c.name);
         img.setAttribute("src", c.image_url);
-        thumbnail.append([caption, img]);
-        return thumbnail;
+        thumbnail.append(caption, img);
+        docFragment.append(thumbnail);
     })
-    document.getElementById("App").append(images);
+
+    document.getElementById("App").appendChild(docFragment);
 }
