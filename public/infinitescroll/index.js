c39414046b6e034ff266f0e10d04841c204c5d4c
Pascal Fong Kye <pfongkye@yahoo.com>
2020-11-02 21:22:38 +0000

    fix: return thumbnails

--- public/infinitescroll/index.js
+++ public/infinitescroll/index.js
@@ -2,13 +2,14 @@ window.onload = async function () {
     const result = await fetch("https://api.jikan.moe/v3/manga/1/characters");
     const mangaChars = await result.json();
     const images = mangaChars.characters.map(c => {
-        const div = document.createElement("div");
+        const thumbnail = document.createElement("div");
         const caption = document.createElement("caption");
         caption.appendChild(document.createTextNode(c.name));
         const img = document.createElement("img");
         img.setAttribute("alt", c.name);
         img.setAttribute("src", c.image_url);
-        div.append([caption, img]);
+        thumbnail.append([caption, img]);
+        return thumbnail;
     })
     document.getElementById("App").append(images);
 }
