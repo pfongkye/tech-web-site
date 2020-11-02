2bce9ea5b22f714f634f0cbfd7b9c3e05f18ac60
Pascal Fong Kye <pfongkye@yahoo.com>
2020-11-02 20:35:29 +0000

    feat: add skeleton html

---
+++ public/infinitescroll/index.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode("Hello world"));
+    document.getElementById("App").appendChild(div);
+}
