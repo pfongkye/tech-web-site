8dd22a86c82a055bc323ff5bad56cbca205be999
Pascal Fong Kye <pfongkye@yahoo.com>
2020-11-03 10:39:24 +0000

    add error handling and edge case

--- public/infinitescroll/index.js
+++ public/infinitescroll/index.js
@@ -13,22 +13,28 @@ window.onload = async function () {
 }
 
 async function getThumbnails(next) {
-    const result = await fetch(`https://api.jikan.moe/v3/manga/${next}/characters`);
-    const mangaChars = await result.json();
-    //a document fragment is stored in memory, and not added to DOM tree -> perf optimization
-    //https://developer.mozilla.org/en-US/docs/Web/API/Document/createDocumentFragment
-    const docFragment = document.createDocumentFragment();
+    try {
+        const result = await fetch(`https://api.jikan.moe/v3/manga/${next}/characters`);
+        const mangaChars = await result.json();
+        //a document fragment is stored in memory, and not added to DOM tree -> perf optimization
+        //https://developer.mozilla.org/en-US/docs/Web/API/Document/createDocumentFragment
+        const docFragment = document.createDocumentFragment();
 
-    mangaChars.characters.forEach(c => {
-        const thumbnail = document.createElement("figure");
-        const caption = document.createElement("figcaption");
-        caption.appendChild(document.createTextNode(c.name));
-        const img = document.createElement("img");
-        img.setAttribute("alt", c.name);
-        img.setAttribute("src", c.image_url);
-        thumbnail.append(img, caption);
-        docFragment.append(thumbnail);
-    });
+        if (Array.isArray(mangaChars.characters)) {
+            mangaChars.characters.forEach(c => {
+                const thumbnail = document.createElement("figure");
+                const caption = document.createElement("figcaption");
+                caption.appendChild(document.createTextNode(c.name));
+                const img = document.createElement("img");
+                img.setAttribute("alt", c.name);
+                img.setAttribute("src", c.image_url);
+                thumbnail.append(img, caption);
+                docFragment.append(thumbnail);
+            });
+        }
 
-    return docFragment;
+        return docFragment;
+    } catch (e) {
+        console.error(e);
+    }
 }
